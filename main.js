9bdeadc438ebf46f88ac3e0264c4b225769feac0
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-09-30 11:30:20 -0700

    refactor complete. Ready for cleanup and more comments.

--- main.js
+++ main.js
@@ -1,94 +1,92 @@
 "use strict";
 
-var Fizzbuzz = (function (na, nb) {
-
-//list of variables
-  var fbOutputArray = [];
-  var fbOutputTitle = "";
-  var fbOutputString = "";
-  var resultBox = "";
-  var resultTitleContainer = "";
-  var resultContainer = "";
-  var text1 = "";
-  var text2 = "";
-
-//read input fields and clear fields, return variables with contents
-  Read = function() {
-    na = parseInt(document.getElementById('number-one').value);
-    nb = parseInt(document.getElementById('number-two').value);
-    document.getElementById('number-one').value = "";
-    document.getElementById('number-two').value = "";
-    return na, nb;
-  };
-
-//calculate fizzbuzz results, output array of results
-  Calculate = function() {
-    read();
-    console.log("start of constructor na: " + na + " nb: " + nb);
-    fbOutputArray = [];
-    if (na > nb) {
-      console.log("na: " + na + " nb: " + nb);
-      na = na + nb;
-      console.log("na: " + na + " nb: " + nb);
-      nb = na - nb;
-      console.log("na: " + na + " nb: " + nb);
-      na = na - nb;
-    }
-    console.log("na: " + na + " nb: " + nb);
-    for (var i = na; i <= nb; i++) { 
-      var output = ''; 
-      if (i%3 === 0) { 
-        output += 'Fizz'; 
+var Fizzbuzz = (function() {
+  //list of variables
+    var fbOutputArray = [];
+    var fbOutputTitle = "";
+    var fbOutputString = "";
+    var resultBox = "";
+    var resultTitleContainer = "";
+    var resultContainer = "";
+    var text1 = "";
+    var text2 = "";
+  
+  //clear results from screen when second button is pushed
+    var clear = function() {
+      if (resultBox.hasChildNodes()) {
+      resultBox.removeChild(resultBox.childNodes[0]);
+      clear();
+    }};
+    
+  //calculate fizzbuzz results, output array of results
+    var calculate = function(na, nb) {
+      console.log("start of constructor na: " + na + " nb: " + nb);
+      fbOutputArray = [];
+      if (na > nb) {
+        console.log("na: " + na + " nb: " + nb);
+        na = na + nb;
+        console.log("na: " + na + " nb: " + nb);
+        nb = na - nb;
+        console.log("na: " + na + " nb: " + nb);
+        na = na - nb;
       }
-      if (i%5 === 0) {
-        output += 'Buzz'; 
+      console.log("na: " + na + " nb: " + nb);
+      for (var i = na; i <= nb; i++) { 
+        var output = ''; 
+        if (i%3 === 0) { 
+          output += 'Fizz'; 
+        }
+        if (i%5 === 0) {
+          output += 'Buzz'; 
+        }
+        if (output === "") { 
+          output = i; 
+        }
+        fbOutputArray.push(output);
       }
-      if (output === "") { 
-        output = i; 
-      }
-      fbOutputArray.push(output);
-    }
-    return fbOutputArray;
-  };
-
-//write markup with results of fizzbuzz calculation
-  Write = function() {
-    fbOutputTitle = "Start: " + na + " Finish: " + nb;
-    fbOutputString = ""; 
-    for (var j=0; j < (fbOutputArray.length); j++) {
-      fbOutputString += (fbOutputArray[j] + " ");
+      return fbOutputArray;
     };
-    resultBox = document.getElementById("result-box");
-    resultTitleContainer = document.createElement("p");
-    resultContainer = document.createElement("p");
-    text1 = document.createTextNode(fbOutputTitle);
-    text2 = document.createTextNode(fbOutputString);
-    resultTitleContainer.appendChild(text1);
-    resultContainer.appendChild(text2);
-    resultBox.appendChild(resultTitleContainer);
-    resultBox.appendChild(resultContainer);
-  };
 
-//clear results from screen when second button is pushed
-  EmptyBox = function() {
-    if (resultBox.hasChildNodes()) {
-    resultBox.removeChild(resultBox.childNodes[0]);
-    EmptyBox();
-  }};
+  //read input fields and clear fields, return variables with contents
+    Fizzbuzz.prototype.read = function(na, nb) {
+      this.na = na;
+      this.nb = nb;
+      this.fbOutputArray = calculate(na, nb);
+    };
+
+  //write markup with results of fizzbuzz calculation
+    Fizzbuzz.prototype.write = function() {
+      fbOutputTitle = "Start: " + this.na + " Finish: " + this.nb;
+      fbOutputString = ""; 
+      for (var j=0; j < (this.fbOutputArray.length); j++) {
+        fbOutputString += (this.fbOutputArray[j] + " ");
+      };
+      resultBox = document.getElementById("result-box");
+      resultTitleContainer = document.createElement("p");
+      resultContainer = document.createElement("p");
+      text1 = document.createTextNode(fbOutputTitle);
+      text2 = document.createTextNode(fbOutputString);
+      resultTitleContainer.appendChild(text1);
+      resultContainer.appendChild(text2);
+      resultBox.appendChild(resultTitleContainer);
+      resultBox.appendChild(resultContainer);
+    };
+
+    Fizzbuzz.prototype.emptyBox = clear;
 });
 
-
+var fbObject = new Fizzbuzz();
 
 document.getElementById("submit").addEventListener("click", function(e) {
   e.preventDefault();
-  // new Fizzbuzz(document.getElementById('number-one').value,document.getElementById('number-two').value);
-  new Fizzbuzz();
-  // document.getElementById('number-one').value = "";
-  // document.getElementById('number-two').value = "";
+  fbObject.read(parseInt(document.getElementById('number-one').value),parseInt(document.getElementById('number-two').value));
+  document.getElementById('number-one').value = "";
+  document.getElementById('number-two').value = "";
+  fbObject.write();
 });
 
 document.getElementById("clear").addEventListener("click", function(e) {
   e.preventDefault();
-
+  fbObject.emptyBox();
 });
 
