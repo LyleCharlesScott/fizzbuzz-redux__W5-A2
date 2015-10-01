08b7525f5e1f2037fc425979a19ae0f2ae1e256e
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-09-30 17:16:58 -0700

    make variables fizz & buzz variable

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 
 // create two Fizzbuzzes just for sport
 var fbObject = new Fizzbuzz();
-var fbObject2 = new Fizzbuzz();
+var fbObject2 = new Fizzbuzz('Bleep', 'Blorp');
 
 // activate first FizzBuzz input area
 document.getElementById("submit").addEventListener("click", function(e) {
@@ -21,7 +21,7 @@ document.getElementById("clear").addEventListener("click", function(e) {
 // activate second FizzBuzz input area
 document.getElementById("submitb").addEventListener("click", function(e) {
   e.preventDefault();
-  fbObject2.read(parseInt(document.getElementById('number-oneb').value),parseInt(document.getElementById('number-twob').value));
+  fbObject2.read(parseInt(document.getElementById('number-oneb').value), parseInt(document.getElementById('number-twob').value));
   document.getElementById('number-oneb').value = "";
   document.getElementById('number-twob').value = "";
   fbObject2.write(document.getElementById("result-boxb"));
